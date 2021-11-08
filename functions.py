087b6e5f541632e65e1099304d1542a15e32baa5
Radoslava <radoslava.hatalova@gmail.com>
2021-11-08 21:26:14 +0100

    added stocks from S&P 100

---
+++ functions.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import datetime
+import pandas_datareader.data as web
+
+start = datetime.datetime(2021, 1, 1)
+end  = datetime.datetime(2021, 11, 1)
+
+def getStocks(stock_list:list, start:datetime, end:datetime)->pd.DataFrame:
+    '''
+    Returns a dataframe with Adj Close of all stocks in stocks_list
+
+    Parameters:
+    ----------
+    stock_list:list
+        list with stock codes
+    start:datetime
+        starting date
+    end:datetime
+        ending date
+
+    Returns:
+    -------
+    pd.DataFrame
+        returning dataframe
+    '''
+    output = pd.DataFrame()
+
+    for stock in stock_list:
+        output[stock] = web.DataReader(stock, 'yahoo', start, end)['Adj Close']
+    return output.copy()
