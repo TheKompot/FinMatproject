ef4337f75f2facc2933c6df65ae87c04f7953c25
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2021-11-08 22:23:29 +0100

    added a debug mode

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ import pandas_datareader.data as web
 start = datetime.datetime(2021, 1, 1)
 end  = datetime.datetime(2021, 11, 1)
 
-def getStocks(stock_list:list, start:datetime, end:datetime)->pd.DataFrame:
+def getStocks(stock_list:list, start:datetime, end:datetime, debug:bool = False)->pd.DataFrame:
     '''
     Returns a dataframe with Adj Close of all stocks in stocks_list
 
@@ -17,6 +17,8 @@ def getStocks(stock_list:list, start:datetime, end:datetime)->pd.DataFrame:
         starting date
     end:datetime
         ending date
+    debug:bool = False
+        if True then in case of an error it prints stock name and continues else crashes
 
     Returns:
     -------
@@ -26,5 +28,12 @@ def getStocks(stock_list:list, start:datetime, end:datetime)->pd.DataFrame:
     output = pd.DataFrame()
 
     for stock in stock_list:
-        output[stock] = web.DataReader(stock, 'yahoo', start, end)['Adj Close']
+        try:
+            output[stock] = web.DataReader(stock, 'yahoo', start, end)['Adj Close']
+        except KeyError:
+            if debug:
+                print(stock)
+            else:
+                raise KeyError('Stock could not be downloaded')
+            
     return output.copy()
