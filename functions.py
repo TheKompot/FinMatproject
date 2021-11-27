bcac793462407e6b09472a9a68d25ef7471f59ee
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2021-11-27 20:35:31 +0100

    added class for genetic algorithm in functions.py it can create first population

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import datetime
 import pandas_datareader.data as web
 
@@ -33,3 +34,28 @@ def getStocks(stock_list:list, start:datetime, end:datetime)->pd.DataFrame:
 
             
     return output.copy()
+
+class GenAlg:
+
+    def __init__(self, population_size,number_of_stocks, k_crossover, mutation_prob):
+        self.population_size = population_size
+        self.k_crossover = k_crossover
+        self.mutation_prob = mutation_prob
+        self.number_of_stocks = number_of_stocks
+
+        self.random_pop()
+    
+    def random_pop(self):
+        self.population = np.random.rand(self.population_size,self.number_of_stocks)
+        for  i in range(self.population.shape[0]):
+            self.population[i,] = self.normalize(self.population[i,])
+    
+    def normalize(self,vector):
+        sum_of_vector = sum(vector)
+        for i in range(len(vector)):
+            vector[i] =  vector[i]/sum_of_vector
+        return vector
+
+# g = GenAlg(5, 10, 0, 0)
+# for i in g.population:
+#     print(sum(i))
